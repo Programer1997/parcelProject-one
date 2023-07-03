94ba1e015366427f5c99c26835d2e89fd63d15b4
luis abraham velazquez angulo <luisabraham.velazquez21@gmail.com>
2023-07-03 16:03:03 -0400

    commit de toda la carpeta con archivos de configuracion

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('hola mundo desde parcel');
+console.log('hola mundo desde parcel2');
+
+import { saludar } from "./modulo.js";
+
+console.log(saludar());
+
+import './estilos/main.scss';
---
+++ src/modulo.js
@@ -0,0 +1,5 @@
+const saludar = ()=> {
+    return 'hola mundo desde un modulo';
+}
+
+export {saludar}
